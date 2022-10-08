bc9a08e91d61f8abc56d0c79f045e48f5ec2395a
vu <31680719+hoangvu0202@users.noreply.github.com>
2022-10-08 19:33:49 +0700

    computer
    first

---
+++ nlv/src/main/java/com/computer/nlv/NlvApplication.java
@@ -0,0 +1,13 @@
+package com.computer.nlv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlvApplication.class, args);
+	}
+
+}
---
+++ nlv/src/test/java/com/computer/nlv/NlvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computer.nlv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
